1ffa6160eb7ca108159a1be77e5d1f31dc19ac2c
thymargo <thymargo@gmail.com>
2019-08-06 16:58:51 +0800

    Freemarker attempt

--- hello/src/main/java/hello/HelloApplication.java
+++ hello/src/main/java/hello/HelloApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+
+
 import java.util.Arrays;
 
 
@@ -17,14 +19,14 @@ public class HelloApplication {
 	}
 	
 	
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
-			System.out.println("Lets inspect the beans");
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			for(String beanName:beanNames)
-				System.out.println(beanName);
-		};
-	}
+//	@Bean
+//	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+//		return args -> {
+//			System.out.println("Lets inspect the beans");
+//			String[] beanNames = ctx.getBeanDefinitionNames();
+//			Arrays.sort(beanNames);
+//			for(String beanName:beanNames)
+//				System.out.println(beanName);
+//		};
+//	}
 }
--- hello/src/main/java/hello/HelloController.java
+++ hello/src/main/java/hello/HelloController.java
@@ -1,6 +1,8 @@
 package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController<Greeting> {
 	
 	
-	@RequestMapping("/")
-	public String index() {
-		return "Hello World";
+	@RequestMapping({"/","/hello"})
+	public String index(Model model, @RequestParam(value="name", defaultValue="World") String name) {
+		model.addAttribute("name",name);
+		return "Hello";
 	}
 }
