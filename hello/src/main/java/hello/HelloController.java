ea292c7452b76035c44c7e8babb7e1dfb9767249
thymargo <thymargo@gmail.com>
2019-08-06 14:28:49 +0800

    RESTful greeting

--- hello/src/main/java/hello/HelloController.java
+++ hello/src/main/java/hello/HelloController.java
@@ -1,10 +1,14 @@
 package hello;
 
+import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class HelloController<Greeting> {
+	
+	
 	@RequestMapping("/")
 	public String index() {
 		return "Hello World";
