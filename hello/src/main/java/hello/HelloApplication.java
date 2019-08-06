ed9aa026f97ed066c0821d19d2ef72e73a95928f
thymargo <thymargo@gmail.com>
2019-08-06 11:05:48 +0800

    code done but testing the maven

--- hello/src/main/java/hello/HelloApplication.java
+++ hello/src/main/java/hello/HelloApplication.java
@@ -1,7 +1,13 @@
 package hello;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import java.util.Arrays;
+
+
 
 @SpringBootApplication
 public class HelloApplication {
@@ -9,5 +15,16 @@ public class HelloApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
 	}
-
+	
+	
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Lets inspect the beans");
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for(String beanName:beanNames)
+				System.out.println(beanName);
+		};
+	}
 }
